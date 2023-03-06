0ba8e50768f537fc7309a604baeebd70dded8a35
Tyler Spear <tylerspear@Tylers-iMac.local>
2023-03-06 10:28:11 -0600

    evens_in_array.js for 3.6.23

---
+++ evens_in_array.js
@@ -0,0 +1,19 @@
+// Given an array of digital numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+
+// For example:
+// ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
+// ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
+// ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
+
+function returnEvens(arr, n) {
+    //filtering the arr down to only evens
+    let evens = arr.filter(el => el % 2 == 0)
+
+    //n number of items from the end
+    let last = evens.slice(evens.length - n)
+    //return those nums in an array
+    return last
+}
+
+console.log(returnEvens([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))
+console.log(returnEvens([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2))
